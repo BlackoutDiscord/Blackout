ca27475a1518bd5b538b99dd596fdff9c13d7d58
beryll1um <vitalii.babichuk@proton.me>
2023-02-28 03:36:52 +0100

    Setup base Typescript build/execution environment
    Description:
    Setup NPM project type with `typescript` and `@types/node`
    dependencies and ES2022 build target.
    
    Test Plan:
    - Rebuild development container
    - Make sure project prepared successfully

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Copyright (c) 2023 beryll1um
+// Distributed under the Unlicense software license, see
+// the accompanying file LICENSE or https://unlicense.org.
+
+console.log("Hello World");
+
