0fddc74fbbacc56870b9c667956c254d50e860db
beryll1um <vitalii.babichuk@proton.me>
2023-03-17 01:27:03 +0100

    Implement discord channel dispatcher
    Description:
    Implement discord channel dispatcher and test channels.
    
    Requirements:
    - Templating with the ability to create categories
      and assign channels to them
    - Ability to create channels and categories out of template
    - Ability to create dispatchers that will handle temporary
      channels that will be deleted on detach
    
    Test Plan:
    1. Start discord bot
    2. Make sure all channels was created
    3. Wait for 5sec
    4. Make sure `Voice` channel was deleted
    5. Stop discord bot
    6. Make sure all channels was deleted
    7. Repeat all steps but skip step 3

---
+++ src/dispatchers/DiscordChannelDispatcher.ts
@@ -0,0 +1,93 @@
+// Copyright (c) 2023 beryll1um
+// Distributed under the Unlicense software license, see
+// the accompanying file LICENSE or https://unlicense.org.
+
+import { ChannelType as DiscordChannelType, GuildChannelCreateOptions, CategoryChannel, GuildChannel, Snowflake } from "discord.js";
+import { DiscordGuildDispatcher, Guild, DISPATCHER_NOT_INITIALIZED_ERROR } from "@interfaces/DiscordGuildDispatcher";
+
+type GuildChannelId = GuildChannel["id"];
+type GuildChannelWithoutId = OmitUnion<GuildChannel, "id">;
+
+type DiscordChannelCreateOptions = Omit<GuildChannelCreateOptions, "parent"> & {
+	parent?: string;
+};
+
+type DiscordChannelDispatcherOptions = {
+	templates?: readonly DiscordChannelCreateOptions[];
+	temporary?: boolean;
+};
+
+type DiscordChannel = Readonly<Pick<GuildChannel, "id" | "name" | "type"> & {
+	parent: DiscordChannel | null;
+}>;
+
+const CHANNEL_DISPATCHER_NOT_FOUND_ERROR = "The dispatcher channel is not found.";
+
+class DiscordChannelDispatcher implements DiscordGuildDispatcher {
+	private _options?: DiscordChannelDispatcherOptions;
+	private _channels: Map<GuildChannelId, GuildChannelWithoutId> = new Map();
+	private _guild?: Guild;
+
+	constructor(options?: DiscordChannelDispatcherOptions) {
+		this._options = options;
+	}
+
+	public async attach(guild: Guild): Promise<void> {
+		this._guild = guild;
+		await this._createChannels();
+	}
+
+	public async detach(): Promise<void> {
+		if (this._options?.temporary) await this._deleteChannels();
+		this._guild = undefined;
+	}
+
+	public async create(template: DiscordChannelCreateOptions): Promise<DiscordChannel> {
+		if (!this._guild) throw new Error(DISPATCHER_NOT_INITIALIZED_ERROR);
+		let parent: Snowflake | undefined;
+		if (template.parent) {
+			parent = this._guild.channels.cache.find((channel) => {
+				return channel.type === DiscordChannelType.GuildCategory && channel.name === template.parent;
+			})?.id;
+		}
+		const channel = await this._guild.channels.create({ ...template, parent });
+		this._channels.set(channel.id, channel);
+		return channel;
+	}
+
+	public async delete(id: DiscordChannel["id"]): Promise<void> {
+		if (!this._guild) throw new Error(DISPATCHER_NOT_INITIALIZED_ERROR);
+		const channel = this._channels.get(id);
+		if (!channel) throw new Error(CHANNEL_DISPATCHER_NOT_FOUND_ERROR);
+		this._channels.delete(id);
+		await channel.delete();
+	}
+
+	public channels(): DiscordChannel[] {
+		return [ ...this._channels.entries() ].map(([id, data]) => ({ id, ...data }));;
+	}
+
+	private async _createChannels(): Promise<void> {
+		if (!this._guild) throw new Error(DISPATCHER_NOT_INITIALIZED_ERROR);
+		const templates = this._options?.templates;
+		if (!templates) return;
+		const [categories, remaining]: [DiscordChannelCreateOptions[], DiscordChannelCreateOptions[]] = [[], []];
+		for (const template of templates) {
+			if (template.type === DiscordChannelType.GuildCategory) {
+				categories.push(template);
+			} else {
+				remaining.push(template);
+			}
+		}
+		await Promise.all(categories.map(category => this.create(category)));
+		await Promise.all(remaining.map(remaining => this.create(remaining)));
+	}
+
+	private async _deleteChannels(): Promise<void> {
+		if (!this._guild) throw new Error(DISPATCHER_NOT_INITIALIZED_ERROR);
+		await Promise.all([ ...this._channels.keys() ].map(id => this.delete(id)));
+	}
+};
+
+export { DiscordChannelCreateOptions, DiscordChannelType, DiscordChannelDispatcherOptions, DiscordChannel, DiscordChannelDispatcher };
+
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@
 
 import { Client, GatewayIntentBits, Interaction } from "discord.js";
 import { CommandInteraction, DiscordCommandDispatcher } from "@dispatchers/DiscordCommandDispatcher";
+import { DiscordChannelDispatcher, DiscordChannelType } from "@dispatchers/DiscordChannelDispatcher";
 
 const commandDispatcher = new DiscordCommandDispatcher([
 	[
@@ -19,10 +20,27 @@ const commandDispatcher = new DiscordCommandDispatcher([
 	]
 ]);
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const channelDispatcher = new DiscordChannelDispatcher({
+	templates: [
+		{
+			name: "Global",
+			type: DiscordChannelType.GuildCategory
+		},
+		{
+			name: "Text",
+			parent: "Global",
+			type: DiscordChannelType.GuildText,
+			topic: "For text communication"
+		}
+	],
+	temporary: true
+});
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers ] });
 
 const stopBot = async () => {
 	await commandDispatcher.detach();
+	await channelDispatcher.detach();
 	client.destroy();
 	console.log("Bot stopped.");
 	process.exit();
@@ -38,7 +56,17 @@ client.on("ready", async () => {
 		stopBot();
 		return;
 	}
-	commandDispatcher.attach(guild);
+	await commandDispatcher.attach(guild);
+	await channelDispatcher.attach(guild);
+	const channel = await channelDispatcher.create({
+		name: "Voice",
+		parent: "Global",
+		type: DiscordChannelType.GuildVoice,
+		userLimit: 10
+	});
+	setTimeout(() => {
+		channelDispatcher.delete(channel.id);
+	}, 5000);
 });
 
 client.on("interactionCreate", (interaction: Interaction) => {
