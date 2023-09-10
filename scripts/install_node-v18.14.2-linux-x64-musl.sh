360cedaaa70e926f643f6785f6bc7cc47b7b086c
beryll1um <vitalii.babichuk@proton.me>
2023-09-10 04:36:20 +0200

    Change of the project license to MIT
    Description:
    The project's main license has been changed due to the site
    being shut down and what appears to be a lack of community
    support.

--- scripts/install_node-v18.14.2-linux-x64-musl.sh
+++ scripts/install_node-v18.14.2-linux-x64-musl.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Copyright (c) 2023 beryll1um
-# Distributed under the Unlicense software license, see
-# the accompanying file LICENSE or https://unlicense.org.
+# Distributed under the MIT software license, see the accompanying
+# file LICENSE or http://www.opensource.org/licenses/mit-license.php
 
 PKG_NAME=node
 PKG_VERSION=v18.14.2
--- src/dispatchers/DiscordCommandDispatcher.ts
+++ src/dispatchers/DiscordCommandDispatcher.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2023 beryll1um
-// Distributed under the Unlicense software license, see
-// the accompanying file LICENSE or https://unlicense.org.
+// Distributed under the MIT software license, see the accompanying
+// file LICENSE or http://www.opensource.org/licenses/mit-license.php
 
 import {
   ChatInputApplicationCommandData,
